219683e98ab7c5815f8d33dd148b56d517cc61d6
Rustam <rustam@MacBook-Pro-Rustam.local>
2022-11-05 18:01:42 +0600

    add div input-wrapper

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
      <h1>React Todo App</h1>
+     <div className="input-wrapper">
+      <input type="text" placeholder="Create a new todo"/>
+      <button className="add-button">Add</button>
+     </div>
     </div>
   );
 }
